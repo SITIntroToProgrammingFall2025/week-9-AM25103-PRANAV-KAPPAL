19185f37362d29346b8c21cecb5b20d0d357c0ab
AM25103-PRANAV-KAPPAL <am25103@sic.shibaura-it.ac.jp>
2025-12-10 02:41:54 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    float a[2][2];
+    float det;
+    int i, j;
+
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%f", &a[i][j]);
+        }
+    }
+
+    printf("You entered\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%.0f ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    det = (a[0][0] * a[1][1]) - (a[0][1] * a[1][0]);
+
+
+    printf("Determinant is %f\n", det);
+
+    return 0;
+}
